0ff59859644815965f8d4c979012dcb1d0d7d759
Kushima <allisonkushima@gmail.com>
2018-05-07 23:30:20 -0300

    Adicionando configuração docker no módulo de recarga

--- recarga/src/main/java/org/edu/kushima/recarga/service/RecargaService.java
+++ recarga/src/main/java/org/edu/kushima/recarga/service/RecargaService.java
@@ -12,7 +12,7 @@ public class RecargaService {
 		
 		StringBuilder sb = new StringBuilder();
 		try {
-			return sb.append("Service: ").append(id).append(" Host: ").append(InetAddress.getLocalHost().getHostName()).toString();
+			return sb.append("Service Recarga: ").append(id).append(" Host: ").append(InetAddress.getLocalHost().getHostName()).toString();
 		} catch (UnknownHostException e) {
 			return sb.append("Erro ao recuperar Recarga. Erro: [").append(e.getMessage()).append("]").toString();
 		}
